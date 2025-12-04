05fe9f540235d2267ccdc5b4233cfc299ef979c4
jluis <jluis.cza@gmail.com>
2025-12-04 11:53:35 -0400

    Creating the Session mmodel and implementing the logout function.

--- src/controllers/docugen-web/admissionController.js
+++ src/controllers/docugen-web/admissionController.js
@@ -1,6 +1,11 @@
 import Account from "../../models/docugen-web/Account.js";
-import { USERS } from "../../constants/users.js"; 
+import Session from "../../models/docugen-web/Session.js";
+import { USERS } from "../../constants/users.js";
 
+// ******************************************************************************
+// SIGNING UP PROCESS
+// It asummes a developer is signing up so a developer role account is created
+// ******************************************************************************
 export const accountRegister = async (req, res) => {
  try {
   const arrivingAccount = req.body;
@@ -16,9 +21,18 @@ export const accountRegister = async (req, res) => {
   }
   const newAccount = new Account(arrivingAccount);
   const accountCreated = await newAccount.saveAccount();
-  const accountCreatedUpdated1 = await Account.setAccountRole(accountCreated._id, USERS.type.client.role.developer); // Giving the default "developer role"
-  const accountCreatedUpdated2 = await Account.setAccountStatus(accountCreated._id, USERS.status.active); //Default active
-  const accountCreatedUpdated3 = await Account.setAccountServices(accountCreated._id, [USERS.services.edition, USERS.services.generation]); //Default both services enabled
+  const accountCreatedUpdated1 = await Account.setAccountRole(
+   accountCreated._id,
+   USERS.type.client.role.developer
+  ); // Giving the default "developer role"
+  const accountCreatedUpdated2 = await Account.setAccountStatus(
+   accountCreated._id,
+   USERS.status.active
+  ); //Default active
+  const accountCreatedUpdated3 = await Account.setAccountServices(
+   accountCreated._id,
+   [USERS.services.edition, USERS.services.generation]
+  ); //Default: both services enabled
 
   if (accountCreated) {
    return res.status(201).json({
@@ -35,12 +49,15 @@ export const accountRegister = async (req, res) => {
   });
  }
 };
+// ******************************************************************************
 
+// ******************************************************************************
+// LOGIN PROCESS
+// ******************************************************************************
 export const sessionStarter = async (req, res) => {
  try {
   const arrivingCredentials = req.body;
 
-  //--> Access authority
   // Checking if there is a registered account with the email or username
   const registeredAccount = await Account.findAccount(
    arrivingCredentials.email,
@@ -52,7 +69,8 @@ export const sessionStarter = async (req, res) => {
     message: "It doesn´t exist an account asociated with this email.",
    });
   }
-  //Verifying password
+
+  // Verifying password
   const isMatch = await registeredAccount.verifyAccountPassword(
    arrivingCredentials.password
   );
@@ -61,23 +79,40 @@ export const sessionStarter = async (req, res) => {
     .status(400)
     .json({ success: false, message: "Incorrect password." });
   }
-  //Preparing response
-  if (registeredAccount && isMatch) {
-   // generating token
-   const token = registeredAccount.generateAccountToken();
-   if (!token) {
-    return res
-     .status(500)
-     .json({ success: false, message: "Error generating the token." });
-   }
-   // sending response
-   return res.status(200).json({
-    success: true,
-    message: "Welcome",
-    token: token,
-    accountData: registeredAccount,
-   });
+
+  // Creating a new session
+  // Creating a new session - Checking if there is a current  ongoing session
+  const currentSession = await Session.findCurrentSession(
+   registeredAccount._id
+  );
+  if (currentSession) {
+   // Closing the ongoing session
+   currentSession.endSession();
   }
+  // Creating a new session - Creating a new session
+  const newSession = new Session();
+  const createdSession = newSession.createSession(registeredAccount._id);
+  if (!createdSession) {
+   return res
+    .status(500)
+    .json({ success: false, message: "A new session hasn't been created." });
+  }
+
+  // Processing response
+  // Processing response - Generating token
+  const token = registeredAccount.generateAccountToken();
+  if (!token) {
+   return res
+    .status(500)
+    .json({ success: false, message: "Error generating the token." });
+  }
+  // Processing response - Sending response
+  return res.status(200).json({
+   success: true,
+   message: "Welcome",
+   token: token,
+   accountData: registeredAccount,
+  });
  } catch (error) {
   res.status(500).json({
    success: false,
@@ -85,4 +120,40 @@ export const sessionStarter = async (req, res) => {
    error: error.message,
   });
  }
+};
+// ******************************************************************************
+
+// ******************************************************************************
+// LOGOUT PROCESS
+// ******************************************************************************
+export const sessionCloser = async (req, res) => {
+ const arrivingIdentity = req.body;
+ console.log({ reqBody: req.body });
+ // Checking if there is a registered account with the username
+ const registeredAccount = await Account.findAccount(
+  "",
+  arrivingIdentity.username
+ );
+ if (!registeredAccount) {
+  return res.status(400).json({
+   success: false,
+   message: "It doesn´t exist an account asociated with this email.",
+  });
+ }
+ // Creating a new session - Checking if there is a current  ongoing session
+ const currentSession = await Session.findCurrentSession(registeredAccount._id);
+ if (!currentSession) {
+  return res.status(400).json({
+   success: false,
+   message:
+    "Current session for the account hasn't been found. The session has been closed.",
+  });
+ }
+ // Closing the ongoing session
+ currentSession.endSession();
+
+ return res.status(200).json({
+  success: true,
+  message: "Session closed.",
+ });
 };
--- src/routes/docugen-web/admissionRoutes.js
+++ src/routes/docugen-web/admissionRoutes.js
@@ -9,9 +9,13 @@ const REGISTER_ACCOUNT_ROUTE =
 
 const LOGIN_USER_ROUTE =
  SERVICES.backend.routes.docugen_web.admission.start_session;
+
+ const LOGOUT_USER_ROUTE =
+ SERVICES.backend.routes.docugen_web.admission.close_session;
 //Endpoints definition
 
 router.post(REGISTER_ACCOUNT_ROUTE, admissionController.accountRegister);
 router.post(LOGIN_USER_ROUTE, admissionController.sessionStarter);
+router.post(LOGOUT_USER_ROUTE, admissionController.sessionCloser);
 
 export default router;
