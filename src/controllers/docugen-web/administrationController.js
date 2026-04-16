511e3fdcc90d97e01c22ea1f638ab189803e1169
jluis <jluis.cza@gmail.com>
2026-04-16 12:09:33 -0400

    Fixed url param management issue

--- src/controllers/docugen-web/administrationController.js
+++ src/controllers/docugen-web/administrationController.js
@@ -32,8 +32,7 @@ export const systemParametersGetter = async (req, res, next) => {
 // ************* System parameter configuration *************
 export const systemParameterSetter = async (req, res, next) => {
   const config = req.body;
-  const id = req.params['system-parameter-id'];
-  console.log({id})
+  const id = req.params['system_parameter_id'];
   const role = req.user.role;
   if (role !== USERS.server.role.administrator) throw new Error('E0502'); // Checking user's role (admin needed)
   try {
